77a3cf2a5e897476bcf7d9acc0561eb268abab04
Divya Gupta <divygupta0319@gmail.com>
2024-06-23 23:24:54 +0530

    Added Authorisation code in SpringDoc config

--- src/main/java/com/sample/fruitsellingweb/configs/SpringDocConfig.java
+++ src/main/java/com/sample/fruitsellingweb/configs/SpringDocConfig.java
@@ -1,16 +1,32 @@
 package com.sample.fruitsellingweb.configs;
 
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springdoc.core.customizers.OpenApiCustomizer;
 import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Paths;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
 public class SpringDocConfig {
+	
+	private static final Logger logger = LoggerFactory.getLogger(SpringDocConfig.class);
+	
+	@Autowired
+	private JwtConfig jwtConfig;
+
 	
 	@Bean
 	public GroupedOpenApi api() {
@@ -24,6 +40,9 @@ public class SpringDocConfig {
 	@Bean
     public OpenAPI fruitStoreOpenAPI() {
         OpenAPI openAPI = new OpenAPI()
+        		.addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+                .components(new Components().addSecuritySchemes("bearerAuth", createJWTAuthScheme())) // for JWT Token
+                .security(List.of(new SecurityRequirement().addList("bearerAuth")))
                 .info(new Info().title("Sample Fruit Store")
                         .description("For SpringDoc config")
                         .version("1.0.0")
@@ -34,5 +53,44 @@ public class SpringDocConfig {
                 
         return openAPI;
     }
+	
+	@Bean
+	public OpenApiCustomizer customizeOpenApi() {
+        return openApi -> {
+ 
+            Paths paths = openApi.getPaths();
+            if (paths != null) {
+                paths.forEach((path, pathItem) -> {
+                    pathItem.readOperations().forEach(operation -> {
+                        if ("generateToken".equals(operation.getOperationId())) {
+                            int validityInMin = jwtConfig.getJwtTokenValidityInMin();
+                            String summary = "Generates a unique JWT auth token, valid for " + validityInMin + " min.";
+
+                            if (validityInMin > 59) {
+                                int hours = validityInMin / 60;
+                                int mins = validityInMin % 60;
+                                
+                                if(mins == 0) {
+                                	summary = "Generates a unique JWT auth token, valid for " + hours + " hrs ";
+                                }
+                                else {
+                                	summary = "Generates a unique JWT auth token, valid for " + hours + " hrs " + mins + " mins";
+                                }
+                            }
+                            logger.info("Updating summary for generateToken: {}", summary);
+                            operation.setSummary(summary);
+                        }
+                    });
+                });
+            }
+        };
+    }
+
+	private SecurityScheme createJWTAuthScheme() {
+		return new SecurityScheme().type(SecurityScheme.Type.APIKEY) // APIKEY or HTTP(depending on requirement)
+				.name("Authorization").description("JWT auth description")
+//    			.bearerFormat("JWT")
+				.in(SecurityScheme.In.HEADER);
+	}
 
 }
