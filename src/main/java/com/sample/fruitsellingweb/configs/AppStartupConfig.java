68c686f6a07387f1378876e7c9d88d7bbc88a0c7
Divya Gupta <divygupta0319@gmail.com>
2024-06-23 21:01:07 +0530

    Setting excluded urls, authenticated urls and token validity from config class

--- src/main/java/com/sample/fruitsellingweb/configs/AppStartupConfig.java
+++ src/main/java/com/sample/fruitsellingweb/configs/AppStartupConfig.java
@@ -1,7 +1,6 @@
 package com.sample.fruitsellingweb.configs;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,9 +10,6 @@ public class AppStartupConfig {
 	
 	@Autowired
     private JwtConfig jwtConfig;
-	
-	@Value("${jwt.token.validity}")
-	private int jwtTokenValidityInMin;
 
     @Bean
     public JwtConfig configureJwtParameters() {
@@ -21,29 +17,19 @@ public class AppStartupConfig {
         jwtConfig.setSecretKey("adaafaasfdaadfAPKFSKPHJUGHJUYGGassdsffffKJJHGHGGJJHHGFSASDDSDA");
 
         // Set token validity in minutes
-//        jwtConfig.setJwtTokenValidityInMin(10 * 60);
-        jwtConfig.setJwtTokenValidityInMin(jwtTokenValidityInMin);
+        jwtConfig.setJwtTokenValidityInMin(10);
 
         // Set the custom authorization header name
         jwtConfig.setAuthHeaderName("Authorization");
         
         // Set the custom token prefix
-        jwtConfig.setRequiredTokenPrefix("JWT");
+        jwtConfig.setRequiredTokenPrefix("Bearer");
 
         // Add excluded URLs
-        jwtConfig.addExcludedUrl("/");
-        jwtConfig.addExcludedUrl("/auth/login");
-        jwtConfig.addExcludedUrl("/payment/paymentCallback");
-        jwtConfig.addExcludedUrl("/platformpayment/swagger-ui/*");
-        jwtConfig.addExcludedUrl("/v3/api-docs/*");
-        jwtConfig.addExcludedUrl("/swagger-resources/**");
-        jwtConfig.addExcludedUrl("/swagger-ui/*");
-        jwtConfig.addExcludedUrl("/webjars/**");
-        jwtConfig.addExcludedUrl("/favicon.ico");
-        jwtConfig.setRootExcluded(true);
+        jwtConfig.addExcludedUrl("/auth/login");       
 
         // Add authenticated URLs
-        jwtConfig.addAuthenticatedUrl("/payment/**");
+        jwtConfig.addAuthenticatedUrl("/api/fruits/**");
         
         return jwtConfig;
     }
