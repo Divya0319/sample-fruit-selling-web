d56031b0d5a393443e011a7ffe7400479c93277e
Divya Gupta <divygupta0319@gmail.com>
2024-06-23 23:57:21 +0530

    Added status code documentation for each API

--- src/main/java/com/sample/fruitsellingweb/controllers/FruitController.java
+++ src/main/java/com/sample/fruitsellingweb/controllers/FruitController.java
@@ -7,41 +7,50 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sample.fruitsellingweb.commons.CommonApiResponses;
+
 @RestController
 @RequestMapping("/api/fruits")
 public class FruitController {
 
     @GetMapping("/list")
+    @CommonApiResponses
     public String getListOfFruits() {
         return "List of all available fruits";
     }
 
     @GetMapping("/details")
+    @CommonApiResponses
     public String getFruitDetails() {
         return "Details of a specific fruit";
     }
 
     @PostMapping("/add")
+    @CommonApiResponses
     public String addNewFruit() {
         return "New fruit added";
     }
 
     @PutMapping("/update")
+    @CommonApiResponses
     public String updateFruit() {
         return "Fruit updated";
     }
 
     @DeleteMapping("/delete")
+    @CommonApiResponses
     public String deleteFruit() {
         return "Fruit deleted";
     }
 
     @GetMapping("/search")
+    @CommonApiResponses
     public String searchFruits() {
         return "Search results for fruits";
     }
 
     @PostMapping("/purchase")
+    @CommonApiResponses
     public String purchaseFruit() {
         return "Fruit purchased";
     }
