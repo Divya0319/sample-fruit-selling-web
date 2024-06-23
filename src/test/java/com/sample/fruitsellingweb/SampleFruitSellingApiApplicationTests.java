48b878dfb9691983e274b9276cf12dd0250d5004
Divya Gupta <divygupta0319@gmail.com>
2024-06-23 19:07:04 +0530

    Initial commit

---
+++ src/test/java/com/sample/fruitsellingweb/SampleFruitSellingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.fruitsellingweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleFruitSellingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
