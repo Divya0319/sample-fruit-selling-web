cfe3e92fc99fe036e43c67dc3818fa9540a4b913
Divya Gupta <divygupta0319@gmail.com>
2024-06-23 20:51:50 +0530

    Added SecurityConfig, which basically creates a filter chain, we can specify which endpoints required to be authenticated, which are to be excluded from auth, which should be exception handling class, which should be Session creation policy, which should be auth filter class and all, we can pass wildcards in urls if we want a hierarchy of endpoints to be passed

---
+++ src/main/java/com/sample/fruitsellingweb/configs/SecurityConfig.java
@@ -0,0 +1,57 @@
+package com.sample.fruitsellingweb.configs;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.sample.fruitsellingweb.filters.JwtAuthFilter;
+import com.sample.fruitsellingweb.helpers.JwtAuthenticationEntryPoint;
+import com.sample.fruitsellingweb.helpers.UrlPatternConverter;
+
+
+
+@Configuration
+public class SecurityConfig {
+	
+	@Autowired
+	private JwtAuthenticationEntryPoint authEntryPoint;
+	
+	@Autowired
+	private JwtAuthFilter authFilter;
+	
+	@Autowired
+	private JwtConfig jwtConfig;
+	
+	@Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(authorize -> {
+                jwtConfig.getExcludedUrls().forEach(url -> authorize.requestMatchers(url).permitAll());
+                jwtConfig.getAuthenticatedUrls().forEach(url -> authorize.requestMatchers(url).authenticated());
+                authorize.anyRequest().authenticated();
+            })
+            .exceptionHandling(ex -> ex.authenticationEntryPoint(authEntryPoint))
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        
+        List<String> convertedUrls = new ArrayList<>();
+        convertedUrls.addAll(new UrlPatternConverter().processWildCardUrls(jwtConfig.getExcludedUrls()));
+        
+        for(String url : convertedUrls) {
+        	if(!url.equals("/")) {
+        		authFilter.addExcludedUrl(url);
+        	}
+        }
+
+        http.addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+	
+
+}
